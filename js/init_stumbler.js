fb8e1201ffa563dcd384fa2750da039b25654b3b
kerry oconnor <kerryoco@gmail.com>
2024-10-12 02:33:37 -0400

    init

---
+++ js/init_stumbler.js
@@ -0,0 +1,35 @@
+const WEIGHT = 10
+const LOSS = 1
+
+
+
+
+const TARGET = 'motorcycle'
+
+
+
+
+
+// corpus
+const CORPUS = 'abcdefghijklmnoqrstuvwxyz'.split('')
+
+
+
+
+
+
+// generator
+let output = ''
+for( const letter of CORPUS ){
+	if( ){
+
+	 // previous letter - this letter < WEIGHT; output += letter
+	}
+}
+
+
+
+
+// evaluator
+if output - TARGET < LOSS
+  save_weight()
