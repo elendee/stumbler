06928f6b6bc04922abb21267b82a50647e9457dc
kerry oconnor <kerryoco@gmail.com>
2024-10-12 11:32:33 -0400

    init

--- js/init_stumbler.js
+++ js/init_stumbler.js
@@ -1,14 +1,3 @@
-const WEIGHT = 10
-const LOSS = 1
-
-
-
-
-const TARGET = 'motorcycle'
-
-
-
-
 
 // corpus
 const CORPUS = 'abcdefghijklmnoqrstuvwxyz'.split('')
@@ -18,18 +7,55 @@ const CORPUS = 'abcdefghijklmnoqrstuvwxyz'.split('')
 
 
 
-// generator
-let output = ''
-for( const letter of CORPUS ){
-	if( ){
+// training
 
-	 // previous letter - this letter < WEIGHT; output += letter
+const train_model = ( args ) => {
+	const {
+		target,
+		loss,
+	} = args
+
+	const WEIGHTS = []
+
+	let output = ''
+	let max_runs = 100 * 1000
+	let c = 0
+	while( c < max_runs ){
+		const weights = random_weights()
+		const guess = generate_guess( weights )
+		if( distance_from( guess, target ) < loss ){
+			WEIGHTS.push( weights )
+		}
+		c++
+	}
+
+	return WEIGHTS
+
+}
+
+
+const generate_guess = ( weights ) => {
+	for( const letter of CORPUS ){
+		/*
+
+
+
+				THE HARD PART
+
+
+
+		*/
 	}
 }
 
 
 
 
-// evaluator
-if output - TARGET < LOSS
-  save_weight()
+
+// run it
+
+const first_try = train_model({
+	target: 'motorcycle',
+	loss: 1,
+})
+
